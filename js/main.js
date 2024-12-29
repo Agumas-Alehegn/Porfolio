44d4631fca6b6220337c20de543838ad957aee6d
Agumas-Alehegn <156206504+Agumas-Alehegn@users.noreply.github.com>
2024-12-29 19:53:05 +0300

    form revised

--- js/main.js
+++ js/main.js
@@ -52,26 +52,3 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("navbar-fixed");
   }
 });
-
-document
-  .getElementById("contact_form")
-  .addEventListener("submit", async function (event) {
-    event.preventDefault();
-    const form = event.target;
-    const formData = new FormData(form);
-
-    const response = await fetch(form.action, {
-      method: form.method,
-      body: formData,
-      headers: {
-        Accept: "application/json",
-      },
-    });
-
-    if (response.ok) {
-      alert("Your message has been sent!");
-      form.reset();
-    } else {
-      alert("Oops! There was a problem submitting your form.");
-    }
-  });
